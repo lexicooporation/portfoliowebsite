19074ca8923aaec532048b4bc15ea776329c6f1b
lexi <nwankwoifeanyi2004@gmail.com>
2026-06-13 21:31:08 +0100

    main commit2

--- portfoliosite/settings.py
+++ portfoliosite/settings.py
@@ -150,7 +150,7 @@ MAINTENANCE_MODE_TEMPLATE = '503.html'
 CACHES = {
     "default": {
         "BACKEND":  "django_redis.cache.RedisCache",
-        "LOCATION": os.getenv("REDIS_URL", "redis://127.0.0.1:6379/1"),
+        "LOCATION": os.getenv("REDIS_URL", "redis://127.0.0.1:6379/2"),
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         },
